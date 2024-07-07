f2132379259f48a08ff41fe8d90ee912f09c49ed
Berenice Barcenas Mancilla <berenice.barcenas.mancilla@gmail.com>
2024-07-07 10:08:16 -0600

    se agrego la funcion que pasados 5 segundos redireccione a la pantalla de la calculadora

--- application/lib/incio.dart
+++ application/lib/incio.dart
@@ -1,4 +1,7 @@
+import 'dart:async'; // Importa este paquete para usar Future y Timer
+
 import 'package:flutter/material.dart';
+import 'calculator.dart'; // Asegúrate de importar el archivo login.dart correctamente
 
 void main() {
   runApp(MyApp());
@@ -20,37 +23,62 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // Aquí usamos un StatefulWidget para poder iniciar el temporizador
     return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          Container(
-            color: Color(0xFFF9A826), // Color de fondo
+      body: _SplashScreen(), // Usa un widget separado para la pantalla de presentación
+    );
+  }
+}
+
+class _SplashScreen extends StatefulWidget {
+  @override
+  __SplashScreenState createState() => __SplashScreenState();
+}
+
+class __SplashScreenState extends State<_SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Utiliza Future.delayed para esperar 8 segundos antes de navegar a la siguiente pantalla
+    Future.delayed(Duration(seconds: 5), () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => calculator()), // Reemplaza LoginScreen con el nombre correcto de tu clase de pantalla de login
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Container(
+          color: Color(0xFFF9A826), // Color de fondo
+        ),
+        Positioned.fill(
+          child: Image.asset(
+            'assets/images/1.png',
+            fit: BoxFit.cover, // Ajusta la imagen al tamaño de la pantalla
           ),
-          Positioned.fill(
-            child: Image.asset(
-              'assets/images/1.png',
-              fit: BoxFit.cover, // Ajusta la imagen al tamaño de la pantalla
+        ),
+        Center(
+          child: Image.asset('assets/images/3.png'), // Segunda imagen centrada
+        ),
+        Positioned(
+          bottom: 20,
+          left: 0,
+          right: 0,
+          child: Text(
+            'CalcIntegral',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontFamily: 'Merriweather',
+              fontSize: 44,
+              color: Colors.white,
             ),
           ),
-          Center(
-            child: Image.asset('assets/images/3.png'), // Segunda imagen centrada
-          ),
-          Positioned(
-            bottom: 20,
-            left: 0,
-            right: 0,
-            child: Text(
-              'CalcIntegral',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontFamily: 'Merriweather',
-                fontSize: 44,
-                color: Colors.white,
-              ),
-            ),
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
